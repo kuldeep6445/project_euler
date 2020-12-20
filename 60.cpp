28d44847d98e2326fa4b97d97886a8f444a7619e
kuldeep6445 <55747743+kuldeep6445@users.noreply.github.com>
2020-12-20 18:11:14 +0530

    solving 60

--- 60.cpp
+++ 60.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
+#include<cstring>
+#include<vector>
 using namespace std;
+
+const int limit = 100000;
+vector<int> prime1,prime2,prime3,prime4,prime5;
+
+bool get_prime(){
+	bool arr[limit];
+    memset(arr,true,sizeof(arr));
+    for(int i=2;i*i<=limit;i++){
+        if(arr[i]){
+            for(int j=i*i;j<=limit;j+=i)
+                arr[j]=false;
+        }
+    }
+    for(int i=2;i<=limit;i++){
+        if(arr[i]){
+            if(i!=2 && i!=5)
+                prime1.push_back(i);
+        }
+    }
+    
+}
+
 
 int main(){
     
+    get_prime();
+    for(int i=0;i<100;i++)
+        cout<<prime1[i]<<" ";
 }
